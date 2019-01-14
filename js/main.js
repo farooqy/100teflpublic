59b53c3b184a0e061cb37952cbf2de9ce543d1b4
farooqy Desktop <noorfarooqy@gmail.com>
2019-01-13 22:02:14 -0800

    Public page for laravel

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('.search-icon').click(function(){
+		if($('.searchdiv').hasClass('visible'))
+		{
+			$('.searchdiv').css('display','none');
+			$('.searchdiv').removeClass('visible');
+		}
+		else
+		{
+			$('.searchdiv').css('display','inline');
+			$('.searchdiv').addClass('visible');
+		}
+	});
+	$('.menu-icon').click(function(){
+		$('.menuhover').toggle();
+	});
+	$('.toggle-menu').click(function(){
+		$('.menuhover').toggle();
+	});
+});
